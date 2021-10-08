4e487ebb8bfd37c6caebaf0bf545a21c48a8a26b
khoabeotv <khoatv141001@gmail.com>
2021-10-08 16:16:13 +0700

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    // First application
+    {
+      name: "Tracking",
+      script: "server.js",
+      instances: 0,
+      exec_mode: "cluster",
+      env: {},
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: "cc",
+      host: ["10.1.12.13"],
+      ref: "origin/master",
+      repo: "git@github.com:khoabeotv/super-barnacle.git",
+      path: "/home/cc/super-barnacle",
+      "post-deploy": "cd frontend && yarn; yarn build",
+    },
+  },
+};
