d2014430ba6c02d55b1dba86c1bf91326ad1cfed
khoabeotv <khoatv141001@gmail.com>
2022-06-10 18:04:13 +0700

    add bank transfer image

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-  API_BASE: dev ? 'https://localhost:4003/api/v1/barnacle' : 'https://pos.pages.fm/api/v1/barnacle'
+  API_BASE: dev ? 'https://localhost:4003/api/v1' : 'https://pos.pages.fm/api/v1'
 }
